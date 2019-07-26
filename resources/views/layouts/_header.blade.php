5dc3c1ae27860c8c2e12f083c21aa109b8a6ea98
lijiahao <lijh@anchnet.com>
2019-07-26 10:47:09 +0800

    验证修改

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
-        .
-        .
-        .
+        <a class="navbar-brand " href="{{ url('/') }}">
+            Mc-shop
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
         <ul class="navbar-nav navbar-right">
             <!-- 登录注册链接开始 -->
             @guest
